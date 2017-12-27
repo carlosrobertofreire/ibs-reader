eef3e9cf2b00ce0d8b157150117549b97b1716f5
Carlos Roberto Freire <carlosrobertofreire@gmail.com>
2017-12-27 20:56:48 -0200

    Multiple debit statements.

--- src/com/carlosrobertofreire/bsreader/Main.java
+++ src/com/carlosrobertofreire/bsreader/Main.java
@@ -5,26 +5,26 @@ public class Main {
     public static void main(String[] args) {
         String separator = "++++++++++++++++++++++++++++++++";
 
-        String statement = "01/12\t\t\tRSHOP-KONI ARCO V-01/12       \t\t30,90\t-";
-        System.out.println("Original Statement");
-        System.out.println(statement);
+        String[] statements = new String[5];
+        statements[0] = "01/12\t\t\tRSHOP-KONI ARCO V-01/12       \t\t30,90\t-";
+        statements[1] = "01/12\t\t\tRSHOP-FETRANSPOR -01/12       \t\t20,00\t-";
+        statements[2] = "01/12\t\t\tRSHOP-SORVETE IT-01/12       \t\t3,50\t-";
+        statements[3] = "04/12\t\t\tRSHOP-COPACABANA -04/12       \t\t39,60\t-";
+        statements[4] = "04/12\t\t\tRSHOP-PAG*Magazin-04/12       \t\t53,50\t-";
 
-        System.out.println(separator);
-        System.out.println("Statement Parts");
-        String[] statementParts = statement.split("\t");
-        for (int i = 0; i < statementParts.length; i++){
-            System.out.println(i + ": " + statementParts[i]);
+        for (String statement : statements){
+            System.out.println(separator);
+            System.out.println("Statement Processed");
+            String[] statementParts = statement.split("\t");
+            String date = statementParts[0];
+            String store = statementParts[3];
+            String value = statementParts[5];
+            String type = statementParts[6].equals("-")?"Debit":"Credit";
+            System.out.println("Date: " + date);
+            System.out.println("Store: " + store.trim());
+            System.out.println("Value: " + value);
+            System.out.println("Type: " + type);
         }
 
-        System.out.println(separator);
-        System.out.println("Statement Processed");
-        String date = statementParts[0];
-        String store = statementParts[3];
-        String value = statementParts[5];
-        String type = statementParts[6].equals("-")?"Debit":"Credit";
-        System.out.println("Date: " + date);
-        System.out.println("Store: " + store.trim());
-        System.out.println("Value: " + value);
-        System.out.println("Type: " + type);
     }
 }
