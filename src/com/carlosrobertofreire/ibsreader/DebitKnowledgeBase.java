105e1f63838de0c2fbc441fcfdc3e9e3eaa5abd7
Carlos Roberto Freire <carlosrobertofreire@gmail.com>
2018-02-14 15:33:16 -0200

    Load items from debit-kb.txt

--- src/com/carlosrobertofreire/ibsreader/DebitKnowledgeBase.java
+++ src/com/carlosrobertofreire/ibsreader/DebitKnowledgeBase.java
@@ -1,5 +1,8 @@
 package com.carlosrobertofreire.ibsreader;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -8,19 +11,38 @@ public class DebitKnowledgeBase {
     private static ArrayList<DebitKnowledgeItem> debitKnowledgeItems;
 
     static {
-        //Create sample data for debitKnowledgeItems
-        DebitKnowledgeItem food = new DebitKnowledgeItem();
-        food.setName("Food");
-        food.setKeywords(new String[]{"KONI", "SAQUE"});
-
-        DebitKnowledgeItem tax = new DebitKnowledgeItem();
-        tax.setName("Tax");
-        tax.setKeywords(new String[]{"IOF"});
+        String userHome = System.getProperty("user.home");
+        String fileName = userHome + "/IBSReader/debit-kb.txt";
 
         debitKnowledgeItems = new ArrayList<DebitKnowledgeItem>();
-        debitKnowledgeItems.add(food);
-        debitKnowledgeItems.add(tax);
 
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            HashMap<String, ArrayList<String>> fileHash = new HashMap<String, ArrayList<String>>();
+            String currentKey = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                if (DebitKnowledgeBase.isNewDebitKnowledgeItem(line)){
+                    currentKey = line;
+                    fileHash.put(line, new ArrayList<String>());
+                } else {
+                    if (!line.isEmpty() && fileHash.containsKey(currentKey)){
+                        fileHash.get(currentKey).add(line);
+                    }
+                }
+            }
+            fileHash.forEach((k, v) -> {
+                DebitKnowledgeItem item = new DebitKnowledgeItem();
+                item.setName(k);
+                item.setKeywords(v);
+                debitKnowledgeItems.add(item);
+            });
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    private static boolean isNewDebitKnowledgeItem(String line) {
+        return line != null && line.startsWith("#");
     }
 
     public static ArrayList<DebitKnowledgeItem> getDebitKnowledgeItems(){
